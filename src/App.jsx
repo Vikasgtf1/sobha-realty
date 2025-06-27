fa6235006f902b13c35b919a21d6bd4e5bcbd486
Hritik <hritik.kumar@gmail.com>
2025-06-27 12:00:05 +0530

    updated  changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl text-amber-300 font-bold underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
