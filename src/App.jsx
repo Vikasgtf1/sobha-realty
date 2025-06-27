d5dd7a1fc059ad71262531cf2d07c18bf5e13984
vikas2489 <vk55002678@gmail.com>
2025-06-27 11:59:17 +0530

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Hero from "./component/Hero";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl text-amber-300 font-bold underline">Hello world!</h1>
+      <Hero />
     </>
   );
 }
