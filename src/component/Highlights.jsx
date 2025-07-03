fc147ee22852517ad98b0cd8b20359b23651037e
kuldeep chaudhary <you@example.com>
2025-07-03 17:09:49 +0530

    update the highlighta

--- src/component/Highlights.jsx
+++ src/component/Highlights.jsx
@@ -73,115 +73,13 @@ const OurHighlightsSection = () => {
       <div className="max-w-7xl mx-auto">
         <div className="mb-16">
           <CommonHeading
-            className=""
-            headingInCenter={false}
+            // className=""
+            // headingInCenter={true}
             heading={"Our Highlights"}
           />
         </div>
 
-        <div className="flex  flex-wrap justify-evenly ">
-          <div className="flex flex-col xl:basis-[26%] basis-[100%] mb-[60px]  xl:mb-[100px]">
-            <div className="text-5xl lg:text-6xl font-light">
-              150k
-              <span className="text-4xl lg:text-5xl font-light ml-2">sqft</span>
-            </div>
-            <p className="text-gray-300  font-helvetica text-sm leading-relaxed  text-left">
-              Of meticulously
-              <br />
-              designed living spaces
-            </p>
-          </div>
 
-          {/* 60% Stat */}
-          <div className="flex flex-col xl:basis-[26%] basis-[100%] mb-[60px]  xl:mb-[100px]">
-            <div className="text-5xl lg:text-6xl font-light">
-              60<span className="text-3xl lg:text-4xl">%</span>
-            </div>
-            <p className="text-gray-300 font-helvetica text-sm leading-relaxed text-left">
-              Green Spaces for
-              <br />
-              tranquility & wellness
-            </p>
-          </div>
-
-          {/* 60% Stat */}
-          <div className="flex flex-col xl:basis-[26%] basis-[100%] mb-[60px]  xl:mb-[100px]">
-            <div className="text-5xl lg:text-6xl font-light">
-              60<span className="text-3xl lg:text-4xl">%</span>
-            </div>
-            <p className="text-gray-300 font-helvetica text-sm leading-relaxed text-left">
-              Green Spaces for
-              <br />
-              tranquility & wellness
-            </p>
-          </div>
-
-          {/* 30 Stat */}
-          <div className="flex flex-col xl:basis-[26%] basis-[100%] mb-[60px]  xl:mb-[100px]">
-            <div className="text-6xl lg:text-7xl font-light">30</div>
-            <p className="text-gray-300 font-helvetica text-sm leading-relaxed max-w-xs ">
-              Exclusive residence
-              <br />
-              each tailored for
-              <br />
-              comfort & wellness
-            </p>
-          </div>
-          {/* 30 Stat */}
-          <div className="flex flex-col xl:basis-[26%] basis-[100%] mb-[60px]  xl:mb-[100px]">
-            <div className="text-6xl lg:text-7xl font-light">30</div>
-            <p className="text-gray-300 font-helvetica text-sm leading-relaxed max-w-xs ">
-              Exclusive residence
-              <br />
-              each tailored for
-              <br />
-              comfort & wellness
-            </p>
-          </div>
-          {/* View More Button */}
-          <div className="flex flex-col xl:basis-[26%] basis-[100%] xl:mb-[100px]">
-            <button className="flex items-center space-x-2 text-white hover:text-gray-300 transition-colors duration-300">
-              <span className="text-sm font-light">View More</span>
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1}
-                  d="M7 17l9.2-9.2M17 17V7H7"
-                />
-              </svg>
-            </button>
-            <div className="w-[100px] mt-[10px]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="110px"
-                height="2"
-                viewBox="0 0 210 2"
-                fill="none"
-              >
-                <path d="M0 0.75H210" stroke="url(#paint0_linear_3627_952)" />
-                <defs>
-                  <linearGradient
-                    id="paint0_linear_3627_952"
-                    x1="0"
-                    y1="1.25"
-                    x2="210"
-                    y2="1.25"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop offset="0.485577" stopColor="white" />
-                    <stop offset="1" stopColor="white" stopOpacity="0" />
-                  </linearGradient>
-                </defs>
-              </svg>
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   );
